502ef6e3d1ca1c156707e44b1ed47c9c1fcd05b6
joseamarcucci <74363457+joseamarcucci@users.noreply.github.com>
2021-08-05 14:04:32 -0300

    Update integration.py

--- integration.py
+++ integration.py
@@ -6,12 +6,12 @@ from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
 message = Mail(
-    from_email='from_email@example.com',
-    to_emails='to@example.com',
+    from_email='info@clayss.org',
+    to_emails='jmarcucci@usal.edu.ar',
     subject='Sending with Twilio SendGrid is Fun',
     html_content='<strong>and easy to do anywhere, even with Python</strong>')
 try:
-    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+    sg = SendGridAPIClient(os.environ.get('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'))
     response = sg.send(message)
     print(response.status_code)
     print(response.body)
